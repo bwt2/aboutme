d3fe0be20ddd78ed21df738f160339c720afe35b
Brian <brian.w.tjahjadi@gmail.com>
2025-02-21 22:36:42 +1100

    layout adjustments and added slidle to navbar

--- app/routes/education/Education.tsx
+++ app/routes/education/Education.tsx
@@ -17,18 +17,7 @@ export default function Education() {
                     <span>The University of Sydney</span> <br/>				                     	                        
                     Feb. 2022 - Jan. 2026 <br/>	
                     Bachelor of Advanced Computing, Majors: Software Development and Physics <br/>				            
-                    Sydney, NSW <br/>	
                     82.10 WAM <br/>	
-                    USYD eSports (SUSOG): player, AAOL'23 Season 4 Division 3 Winner <br/>	
-                    Sydney Computing Society (SYNCS): member <br/>	
-                </p>
-                <p>
-                    <span>IPEKA Integrated Christian School</span> <br/>	
-                    2021 <br/>				                     	                        
-                    High School <br/>				            
-                    Jakarta, Indonesia <br/>	
-                    ATAR: 90.55, SAT: 1470 <br/>	
-                    Best in Mathematics Extension 1 2021 <br/>	
                 </p>
             </main>
         </div>
