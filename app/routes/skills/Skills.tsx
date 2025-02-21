d2c7d3a8ba8c628e606e9772469d6ed7f4c5bb0a
Brian <brian.w.tjahjadi@gmail.com>
2025-02-21 22:11:24 +1100

    experience content simple

--- app/routes/skills/Skills.tsx
+++ app/routes/skills/Skills.tsx
@@ -14,66 +14,59 @@ export default function Skills() {
             <h1>Skills</h1>
             <main>
               <ul className={styles.skillList}>
-                <li className={styles.skillCategory}>
-                  <h2>Frontend</h2>
-                  <ul>
-                    <li>React</li>
-                    <li>Remix / React Router v7</li>
-                    <li>JavaScript</li>
-                    <li>TypeScript</li>
-                    <li>TailwindCSS</li>
-                    <li>D3.js</li>
-                  </ul>
-                </li>
-                <li className={styles.skillCategory}>
-                  <h2>Backend and Databases</h2>
-                  <ul>
-                    <li>Python</li>
-                    <li>Django</li>
-                    <li>FastAPI</li>
-                    <li>SQLAlchemy</li>
-                    <li>PostgreSQL</li>
-                    <li>MySQL</li>
-                  </ul>
-                </li>
-                <li className={styles.skillCategory}>
-                  <h2>DevOps and Tooling</h2>
-                  <ul>
-                    <li>Git</li>
-                    <li>Github</li>
-                    <li>Poetry</li>
-                    <li>Vite</li>
-                    <li>Docker</li>
-                    <li>Linux (WSL2 Ubuntu)</li>
-                  </ul>
-                </li>
-                <li className={styles.skillCategory}>
-                  <h2>Robotics</h2>
-                  <ul>
-                    <li>ROS 2</li>
-                    <li>ROS 2 Control</li>
-                    <li>C++</li>
-                    <li>Python</li>
-                  </ul>
-                </li>
-                <li className={styles.skillCategory}>
-                  <h2>Data Science</h2>
-                  <ul>
-                    <li>Python</li>
-                    <li>numpy</li>
-                    <li>matplotlib</li>
-                    <li>Jupyter</li>
-                  </ul>
-                </li>
-                <li className={styles.skillCategory}>
-                  <h2>Other</h2>
-                  <ul>
-                    <li>Java</li>
-                    <li>C</li>
-                  </ul>
-                </li>
+                <SkillCategory heading={"Frontend"}>
+                  <li>React</li>
+                  <li>Remix / React Router v7</li>
+                  <li>JavaScript</li>
+                  <li>TypeScript</li>
+                  <li>TailwindCSS</li>
+                  <li>D3.js</li>
+                </SkillCategory>
+                <SkillCategory heading={"Backend and Databases"}>
+                  <li>Python</li>
+                  <li>Django</li>
+                  <li>FastAPI</li>
+                  <li>SQLAlchemy</li>
+                  <li>PostgreSQL</li>
+                  <li>MySQL</li>
+                </SkillCategory>
+                <SkillCategory heading={"DevOps and Tooling"}>
+                  <li>Git</li>
+                  <li>Github</li>
+                  <li>Poetry</li>
+                  <li>Vite</li>
+                  <li>Docker</li>
+                  <li>Linux (WSL2 Ubuntu)</li>
+                </SkillCategory>
+                <SkillCategory heading={"Robotics"}>
+                  <li>ROS 2</li>
+                  <li>ROS 2 Control</li>
+                  <li>C++</li>
+                  <li>Python</li>
+                </SkillCategory>
+                <SkillCategory heading={"Data Science"}>
+                  <li>Python</li>
+                  <li>numpy</li>
+                  <li>matplotlib</li>
+                  <li>Jupyter</li>
+                </SkillCategory>
+                <SkillCategory heading={"Other"}>
+                  <li>Java</li>
+                  <li>C</li>
+                </SkillCategory>
               </ul>
             </main>
         </div>
     )
+}
+
+function SkillCategory({heading, children}: {heading: string, children: React.ReactNode | null}) {
+  return (
+    <li className={styles.skillCategory}>
+      <h2>{heading}</h2>
+      <ul>
+        {children}
+      </ul>
+    </li>
+  )               
 }
